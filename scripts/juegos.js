1e51821722802d31dcb2b9d370f5fea7b57fc870
Adria <adrivid98@gmail.com>
2021-02-20 01:20:32 +0100

    V1 juegos

--- scripts/juegos.js
+++ scripts/juegos.js
@@ -1,34 +1,62 @@
 // Objeto con el array de juegos cargado.
-//var listadoJuegos;
+var listadoJuegos;
 
-/**
+/*
  * Función que se ejecuta cuando el DOM se ha cargado completamente
  */
-
 $(() => {
   $.getJSON('../resources/juegos.json', function (data) {
     listadoJuegos = data;
     $(document).trigger('juegosCargados');
-    console.log(listadoJuegos);
+    $('.horizontal').attr('hidden', true);
+    $('.form-check').children().remove();
+    $('.input-group-mb-3').attr('hidden', true);
   });
 });
 
-/**
+/*
  * Evento que se lanza al acabar de cargar los juegos del fichero JSON
  */
-$(() => {
-  $('.horizontal').attr("hidden", true);
-  $('.form-check').children().remove();
+$(document).on('juegosCargados', function () {
+  console.log(listadoJuegos); // test
+
+  $('.form-select').change((event) => {
+    /** Si pulsamos sobre filtrar por genero.. */
+    if ($(event.target).val() == 1) {
+      $('.form-select option[value=1]').attr('hidden', true);
+      $('.form-check').children().remove();
+      let setGeneros = new Set();
+
+      for (let game of listadoJuegos) setGeneros.add(game.genero);
+
+      for (let genero of setGeneros) {
+        let columna = $(
+          "<div class='form-check-div'><input class='form-check-input' type='checkbox' value='" +
+            genero +
+            "'>" +
+            "<label class='form-check-label' for='" +
+            genero +
+            "'>" +
+            genero +
+            '</label></div>'
+        );
+        $('.form-check').append(columna);
+      }
+      /** Si filtramos por año.. */
+    } else if ($(event.target).val() == 2) {
+      
+      $('.form-check').children().remove();
+      $('.input-group-mb-3').attr('hidden', false);
+      $('.input-group-mb-3').attr("pattern", '[0-9]');
+      $('.input-group-mb-3').keypress(function (e) { 
+        if( e.charCode>= 97 && event.charCode <= 122){
+         
+         $('.input-group-mb-3').val.replaceWith(" ");
+        }
+        
+      });
+    }
   });
 
-
-
-$(document).on('juegosCargados', function () {
   
-  
-  
-
- if( $(".form-select option[value="+ 1 +"]").attr("selected",true)){
-
- }
 });
